4c20bf9cebb5a2edf1b523021b32d26617374813
Neal Terrell <neal.terrell@csulb.edu>
2019-01-25 11:59:02 -0800

    Add quadratic and random number examples.

---
+++ Introductions/Quad/quad.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	double a, b, c;
+
+	cout << "Enter a b and c:";
+	cin >> a >> b >> c;
+
+	double descriminant = b * b - 4 * a * c;
+	if (descriminant < 0) {
+		cout << "No real soultions" << endl;
+		return 0;
+	}
+	if (descriminant == 0) {
+		cout << "One solution: x = " << -b / (2 * a) << endl;
+	}
+	else {
+		double sq = sqrt(descriminant);
+		cout << "Two solutions: x1 = " << (-b + sq) / (2 * a)
+			<< "; x2 = " << (-b - sq) / (2 * a) << endl;
+	}
+
+}
---
+++ Introductions/Randoms/rands.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+// For random numbers.
+#include <random>
+
+using namespace std;
+
+int main() {
+	// Create only ONE random_device.
+	random_device rd;
+	// Create only ONE engine.
+	default_random_engine engine(rd());
+
+	// Create a different distribution for each of your needs. The parameters are INCLUSIVE.
+	uniform_int_distribution<int> range(1, 10);
+
+	// Invoke the range with the engine to get a value in the specified range.
+	int r = range(engine);
+	int r2 = range(engine);
+
+	uniform_int_distribution<int> range2(9, 17);
+
+	cout << range(engine) << " " << range2(engine) << endl;
+
+}
